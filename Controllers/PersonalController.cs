091e32867628e023f7ca491f02c4a4c728b17359
Cesar Martinez <oliver-cesar@hotmail.com>
2021-11-08 16:16:32 -0600

    Agree Personal Page

--- Controllers/PersonalController.cs
+++ Controllers/PersonalController.cs
@@ -14,6 +14,11 @@ namespace MyFirstAppASP.Controllers
         public IActionResult Index()
         {
             Personal personal = new Personal();
+            personal.profession = "Student";
+            personal.experience = "1 Year";
+            personal.phone = "+503 7470-8027";
+            personal.email = "SMIS055621@UGB.EDU.SV";
+            personal.location = "Santa Rosa de Lima, La Union, El Salvador, Centroamerica.";
             personal.name ="Cesar";
             personal.lastname ="Martinez";
             personal.age=20;
--- Models/Personal.cs
+++ Models/Personal.cs
@@ -4,6 +4,11 @@ namespace MyFirstAppASP.Models
 {
     public class Personal
     {
+        public string profession;
+        public string experience;
+        public string phone;
+        public string email;
+        public string location;
         public string name;
         public string lastname;
         public int age;
